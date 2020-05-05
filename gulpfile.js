e6e3ac598a2d0b74e773f6d28acd60b1e3c3e244
Maksym <maksym.sivko@gmail.com>
2020-05-05 10:49:03 +0300

    +

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,38 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const cache = require('gulp-cache');
-const imageminJpeg = require('imagemin-jpeg-recompress');
-const pngquant = require('pngquant');
 
 //---------------------------------------------------
 const paths = {
-	img: {
-		src: 'app/imgs/**/*.*',
-		dest: 'app/img'
-	}
+  img: {
+    src: 'app/imgs/**/*.*',
+    dest: 'app/img',
+  },
 };
 
 // IMG tack
 const imgMin = () => {
-	return gulp.src(paths.img.src)
-		.pipe(cache(imagemin([
-			imagemin.gifsicle({
-				interlaced: true
-			}),
-			imagemin.jpegtran({
-				progressive: true
-			}),
-			imageminJpeg({
-				loops: 4,
-				min: 50,
-				max: 95,
-				quality: 'medium'
-			}),
-			imagemin.svgo(),
-			imagemin.optipng({
-				optimizationLevel: 3
-			}),
-			pngquant({
-				quality: '65-70',
-				speed: 5
-			})
-		], {
-			verbose: true
-		})))
-		.pipe(gulp.dest(paths.img.dest))
-}
+  return gulp
+    .src(paths.img.src)
+    .pipe(
+      cache(
+        imagemin(
+          [
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.mozjpeg({ quality: 75, progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+              plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+            }),
+          ],
+          {
+            verbose: true,
+          }
+        )
+      )
+    )
+    .pipe(gulp.dest(paths.img.dest));
+};
 
 const minImg = gulp.parallel(imgMin);
 
